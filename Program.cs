572ffe291261ab1d3148e1d37de4fe25aff077be
Marcin <uT_3777@zsp3zamosc>
2025-05-21 09:54:51 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tttt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank Bank = new Bank(1,100);
+            Bank.wyplata(100);
+          
+            Bank.wplata(1000);
+
+           
+                double stanKonta = Bank.getStanKonta();
+            Console.WriteLine("stan konta:");
+            Console.WriteLine(stanKonta);
+            Console.ReadLine();
+
+            
+            
+        }
+    }
+}
---
+++ bank.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tttt
+{
+    public class Bank
+
+    {
+
+        private double stanKonta;
+
+        public int nrKlienta { get; private set; }
+
+        public Bank(int nrKlienta, double stanKonta = 0)
+
+        {
+
+            this.nrKlienta = nrKlienta;
+
+            this.stanKonta = stanKonta;
+
+        }
+
+        public double getStanKonta() => stanKonta;
+
+
+
+        public bool wplata(double kwota)
+
+        {
+
+
+            if (kwota > 0)
+
+            {
+
+                stanKonta += kwota;
+
+                return true;
+
+            }
+
+            return false;
+
+        }
+
+
+
+        public bool wyplata(double kwota)
+
+        {
+
+            if (kwota <= 0)
+
+                return false;
+
+            if (stanKonta > kwota)
+
+            {
+
+                stanKonta -= kwota;
+
+                return true;
+
+            }
+
+            return false;
+
+        }
+
+    }
+}
+
+
